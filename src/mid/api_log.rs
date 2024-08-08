46724c055098cb20f876fc455763952d7b687348
tigerli <83432087@qq.com>
2024-08-08 16:13:28 +0800

    init

--- src/mid/api_log.rs
+++ src/mid/api_log.rs
@@ -1,11 +1,10 @@
 use axum::{
-    body::{Body, Bytes},
+    body::{to_bytes, Body},
     extract::Request,
     http::StatusCode,
     middleware::Next,
     response::{IntoResponse, Response},
 };
-use http_body_util::BodyExt;
 use std::time::Instant;
 use tracing::info;
 
@@ -16,7 +15,7 @@ pub async fn print_request_response(
     let (parts, body) = req.into_parts();
     let uri = parts.uri.to_string();
     let method = parts.method.to_string();
-    let bytes = get_bytes(body).await?;
+    let bytes = to_bytes(body, usize::MAX).await.unwrap();
     let req_bytes = &bytes.clone();
     let req_val = std::str::from_utf8(req_bytes).unwrap();
     info!("{} {} req:{}", method, uri, req_val);
@@ -25,7 +24,7 @@ pub async fn print_request_response(
     let res = next.run(req).await;
     let duration = start.elapsed();
     let (parts, body) = res.into_parts();
-    let bytes = get_bytes(body).await?;
+    let bytes = to_bytes(body, usize::MAX).await.unwrap();
     let res_body = std::str::from_utf8(&bytes);
     info!(
         "{} {} {:?} req:{} res:{}",
@@ -37,19 +36,4 @@ pub async fn print_request_response(
     );
     let res = Response::from_parts(parts, Body::from(bytes));
     Ok(res)
-}
-
-async fn get_bytes<B>(body: B) -> Result<Bytes, (StatusCode, String)>
-where
-    B: axum::body::HttpBody<Data = Bytes>,
-    B::Error: std::fmt::Display,
-{
-    let bytes = match BodyExt::collect(body).await {
-        Ok(collected) => collected.to_bytes(),
-        Err(err) => {
-            return Err((StatusCode::BAD_REQUEST, format!("failed to body: {err}")));
-        }
-    };
-
-    Ok(bytes)
 }
